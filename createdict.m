275495e4a9e14e1e8e342c6d4d60605ac8d01689
socaro <lohmanns@stanford.edu>
2017-01-13 12:24:43 -0800

    changes in dictlearn and createdict

--- createdict.m
+++ createdict.m
@@ -2,17 +2,24 @@ clear all;
 close all;
 clc;
 images=dir('traindata/*.tif');
-s=16;
-traindata=[];
+%images=dir('traindata/*.jpg');
+s=8;
+traindata=cell(3);
 tic;
 for i=1:length(images)
     image=imread(fullfile('traindata',images(i).name));
     image=imadjust(image,findlim(image,5,95),[0;1]);
-    imcol=[im2col(double(image(:,:,1)),[s s],'distinct') im2col(double(image(:,:,2)),[s s],'distinct') im2col(double(image(:,:,3)),[s s],'distinct')];
-    traindata=[traindata imcol];
+    for i=1:3
+    imcol=im2col(double(image(:,:,i)),[s s],'distinct');
+    imcol=imcol(:,randperm(length(imcol(1,:)),300));
+    traindata{i}=[traindata{i} imcol];
+    end  
 end
 toc;
-dict=dictlearn(traindata,s,50,10);
-save('learneddict.m','dict');
-imagedict=col2im(dict,[s s],size(dict),'distinct');
+for i=1:3
+dict(:,:,i)=dictlearn(traindata{i},s,50,10);
+dim=size(dict(:,:,i));
+imagedict=col2im(dict(:,:,i),[s s],[s^2*3 s^2*2],'distinct');
 imshow(imagedict);
+end
+save('learneddicttest.mat','dict');
--- dictlearn.m
+++ dictlearn.m
@@ -1,5 +1,5 @@
 function dict = dictlearn(traindata, s, iteration,mpiteration)
-    %%SVD algorithm to train a dictionary usind input traindata
+    %%SVD algorithm to train a dictionary using input traindata
     %Input:
     %traindata      - matrix containing one image patch [s s] per column, thus has
     %                 s^2 rows
@@ -9,7 +9,7 @@ function dict = dictlearn(traindata, s, iteration,mpiteration)
     %Output:
     %dict           - learned dictionary
 
-    L=7*s^2;
+    L=6*s^2;
     u=rand([s^2 L]); %using a randomly initialized dictionary
     %u=wmpdictionary(s^2,'lstcpt',{{'haar',2},{'haar',2},{'haar',2},{'haar',2}}); %using a set of level 5 Haar wavelets as dicitonary
     %L=length(u(1,:));
@@ -20,15 +20,21 @@ function dict = dictlearn(traindata, s, iteration,mpiteration)
 %         traindata_mean(:,n)=sum(traindata(:,n))/length(traindata(:,n))*ones(size(traindata(:,n)));
 %     end
 %     traindata=traindata-traindata_mean;
-    for n=1:N
-        traindata(:,n)=traindata(:,n)-mean(traindata(:,n));
-    end
+%     for n=1:N
+%         traindata(:,n)=traindata(:,n)-mean(traindata(:,n));
+%     end
+    traindata=(traindata-mean(traindata(:)))/(std(traindata(:))*sqrt(N));
     for i=1:iteration
         z_new=zeros(L,N);
         tic;
         for n=1:N
-            [~,~,COEFF,IOPT]=wmpalg('BMP',traindata(:,n),u,'itermax',mpiteration);%'maxerr',{'L2',sigma*100});
+            %[Yfit,R,COEFF,IOPT]=wmpalg('BMP',traindata(:,n),u,'itermax',mpiteration);%'maxerr',{'L2',sigma*100});
+            %[Yfit1,R1,COEFF1,IOPT1]=wmpalg('BMP',traindata(:,n),u,'itermax',100);
+            [Yfit,R,COEFF,IOPT]=wmpalg('BMP',traindata(:,n),u,'maxerr',{'L2',5});
             z_new(IOPT,n)=COEFF;
+            z_new2=z_new(IOPT,n);
+            [~,I]=sort(IOPT);
+            z_new1=COEFF(I);
         end
         toc;
         tic;
@@ -41,12 +47,14 @@ function dict = dictlearn(traindata, s, iteration,mpiteration)
 %            [U,~,~]=svd(R_l,'econ');
 %            toc;
            [U1,~]=eig(R_l*ctranspose(R_l));
-           u_new(:,l)=-U1(:,64);
+           u_new(:,l)=-U1(:,length(U1(1,:)));
 	       %disp(fprintf('i: %d, l: %d\n',i,l));
         end 
         toc;
         i
         u=u_new;
+        imagedict=col2im(u,[s s],[s^2*3 s^2*2],'distinct');
+        imshow(imagedict);
     end
     dict=u_new;
 end
